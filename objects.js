93d3c34ec8caade0dc24325fae5a889298b95ee4
Keegan Rankin <kms.rankin@gmail.com>
2018-12-25 13:57:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var songTitle, artistName
 var playlist = {artistName: songTitle}
 playlist.win = "lose"
-function update(playlist, artistName, songTitle) {
-  return playlist.artistName = songTitle
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = songTitle
 }
